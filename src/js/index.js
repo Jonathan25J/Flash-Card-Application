8d259d28ee4e602a00622c826ee565dd37930541
Jonathan25J <ik.ben.jona4@gmail.com>
2024-02-16 23:03:33 +0100

    changed green to blue

--- src/js/index.js
+++ src/js/index.js
@@ -2,5 +2,5 @@ const title = document.querySelector('title');
 title.textContent = api.title;
 
 document.querySelector('#btn').addEventListener('click', () => {
-    document.body.style.backgroundColor = 'green'
+    document.body.style.backgroundColor = 'blue'
 })
