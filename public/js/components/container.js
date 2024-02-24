90f3831f2203727b599684c4e5614f985a273f94
Jonathan25J <ik.ben.jona4@gmail.com>
2024-02-24 16:57:01 +0100

    container in working state but needs fixes

--- public/js/components/container.js
+++ public/js/components/container.js
@@ -15,8 +15,8 @@ export class ContainerElement extends LitElement {
             direction: 'row',
             rows: 3,
             columns: 5,
-            containerRatio: [1, 1, 1],
-            cellRatio: {1: [1, 5, 1, 1, 1]}
+            containerRatio: [1, 2, 3],
+            cellRatio: [1, 5, 1, 2 ,2]
         }
         this.containers = []
     }
@@ -38,21 +38,30 @@ export class ContainerElement extends LitElement {
     generate() {
         this.containers = []
         this.holders = []
+        let direction = this.instructions.direction
+        let containers = (direction === 'row' ? this.instructions.rows : this.instructions.columns)
+        let cells = (direction === 'row' ? this.instructions.columns : this.instructions.rows)
 
-        let direction = this.instructions.direction == 'column' ? 'container-column' : 'container-row'
-        let element1 = this.instructions.direction == 'column' ? 'columns' : 'rows';
-        let element2 = this.instructions.direction == 'column' ? 'rows' : 'columns';
+        this.instructions.containerRatio = this._transformList(this.instructions.containerRatio, containers)
+        this.instructions.cellRatio = this._transformList(this.instructions.cellRatio, cells)
+
+        console.log(this.instructions.cellRatio, this.instructions.containerRatio)
+
+        let directionContainer = direction == 'column' ? 'container-column' : 'container-row'
+        let element1 = direction == 'column' ? 'columns' : 'rows';
+        let element2 = direction == 'column' ? 'rows' : 'columns';
 
 
         for (let i1 = 1; i1 < this.instructions[element1] + 1; i1++) {
             let cells = []
             for (let i2 = 1; i2 < this.instructions[element2] + 1; i2++) {
-                cells.push(html`<div class="slot-container" style="flex: ${this.instructions.cellRatio[i2 - 1]};"><slot name="${element1[0]}${i1}-${element2[0]}${i2}"></slot></div>`)
+                console.log(this.instructions.cellRatio[i1 - 1][`${i1}`][i2 - 1], i2)
+                cells.push(html`<div class="slot-container" style="flex: ${this.instructions.cellRatio[i1 - 1][`${i1}`][i2 - 1]};"><slot name="${element1[0]}${i1}-${element2[0]}${i2}"></slot></div>`)
             }
-            let holder = html`<div class="holder ${element1.slice(0, -1)}" style="flex: ${this.instructions.containerRatio[i1 - 1]};"> ${cells.map(cell => html`${cell}`)}</div>`
+            let holder = html`<div class="holder ${element1.slice(0, -1)}" style="flex: ${this.instructions.containerRatio[i1 - 1][`${i1}`][i1 - 1]};"> ${cells.map(cell => html`${cell}`)}</div>`
             this.holders.push(holder)
         }
-        this.containers = html`<div class="container ${direction}">${this.holders.map(container => html`${container}`)}</div>`
+        this.containers = html`<div class="container ${directionContainer}">${this.holders.map(container => html`${container}`)}</div>`
     }
 
     render() {
@@ -62,9 +71,18 @@ export class ContainerElement extends LitElement {
     `
     }
 
-    // _transformList() {
+    _transformList(list, amount) {
+        if (!Array.isArray(list)) return list
+        let newList = []
+        for (let i = 1; i <= amount; i++) {
+            let currentList = {}
+            currentList[i] = list.slice(0, amount)
+            newList.push(currentList);
+        }
+        return newList
 
-    // }
+
+    }
 
     static get styles() {
         return css`
