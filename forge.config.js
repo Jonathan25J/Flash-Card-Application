156b303d36aadd66488afc3d994a0bbca9f43cde
Jonathan25J <ik.ben.jona4@gmail.com>
2024-02-25 18:18:36 +0100

    config changed for custom icon

--- forge.config.js
+++ forge.config.js
@@ -1,12 +1,19 @@
+import path from 'path';
+
 export const packagerConfig = {
   asar: true,
-  "extraResource": "resources/website"
+  extraResource: path.join(path.resolve(), 'resources/website'),
+  icon: path.join(path.resolve(), 'icon.ico')
 };
+
 export const rebuildConfig = {};
+
 export const makers = [
   {
     name: '@electron-forge/maker-squirrel',
-    config: {},
+    config: {
+      setupIcon: path.join(path.resolve(), 'icon.ico')
+    },
   },
   {
     name: '@electron-forge/maker-zip',
@@ -14,13 +21,14 @@ export const makers = [
   },
   {
     name: '@electron-forge/maker-deb',
-    config: {},
+    config: { icon: path.join(path.resolve(), 'icon.ico') },
   },
   {
     name: '@electron-forge/maker-rpm',
-    config: {},
+    config: { icon: path.join(path.resolve(), 'icon.ico') },
   },
 ];
+
 export const plugins = [
   {
     name: '@electron-forge/plugin-auto-unpack-natives',
