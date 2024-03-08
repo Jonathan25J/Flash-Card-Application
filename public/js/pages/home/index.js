70879b45e4068d8caab6fb98c0a3fb8acd6bc326
Jonathan25J <ik.ben.jona4@gmail.com>
2024-03-08 16:16:10 +0100

    added last themes

--- public/js/pages/home/index.js
+++ public/js/pages/home/index.js
@@ -2,4 +2,4 @@ import '../../components/profile/profilesHolder.js';
 
 import '../../components/themeSwitcher.js';
 
-document.querySelector('theme-switcher').setOptions(['default', 'night-fade', 'deep-blue', 'chrome', 'dense-waters', 'moon'])
+document.querySelector('theme-switcher').setOptions(['default', 'night-fade', 'deep-blue', 'chrome', 'dense-waters', 'moon', 'sun', 'fresh'])
